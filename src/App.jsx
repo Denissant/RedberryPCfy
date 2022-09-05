d2d46ab4286c541086d1c5161ba05227a1d90745
Denissant <denissanturyan@gmail.com>
2022-09-05 14:22:16 +0400

    added routes and validators

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import {Route, Routes} from "react-router-dom";
 import LandingPage from "./pages/Landing";
 import EmployeeForm from "./pages/EmployeeForm.jsx";
 import LaptopForm from "./pages/LaptopForm.jsx";
+import SuccessPage from "./pages/Success";
+import LaptopListPage from "./pages/LaptopList";
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
             <Route path="/" element={<LandingPage/>} />
             <Route path="/new/employee" element={<EmployeeForm />} />
             <Route path="/new/laptop" element={<LaptopForm />} />
+            <Route path="/success" element={<SuccessPage />} />
+            <Route path="/list" element={<LaptopListPage />} />
         </Routes>
     );
 }
--- src/inputValidators.js
+++ src/inputValidators.js
@@ -2,8 +2,12 @@ const validators = {
     lessThanTwo: string => string.length > 1 || 'გამოიყენე მინიმუმ ორი სიმბოლო',
     georgian: string => /^[ა-ჰ]*$/.test(string) || 'გამოიყენე ქართული ასოები',
     laptopName: string => /^[0-9A-Za-z\-!@#$%^&*()_+=]+$/.test(string) || 'ლათინური ასოები, ციფრები, !@#$%^&*()_+=',
+    numbersOnly: string => /^[0-9]*$/.test(string) || 'მხოლოდ ციფრები',
+    date: string => dateRegex.test(string.replace(/\s+/g, '')) || 'ვალიდური თარიღი თთ/დდ/წწწწ ფორმატით',
     phone: string => /^(\+995)(5\d{8})$/.test(string.replace(/\s+/g, '')) || 'ქართული მობ-ნომრის ფორმატი',
     email: string => ( string.endsWith('@redberry.ge') && !string.startsWith('@') ) || 'უნდა მთავრდებოდეს @redberry.ge-ით',
 }
+
+const dateRegex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/
 
 export default validators;
